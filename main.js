112fb1bb02fb37dcc28a7485f8d1d08455a863fe
unknown <kheronn@gmail.com>
2024-03-19 19:54:13 -0300

    Reunião  19/03 R04 - Alterando conteúdo com DOM

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
-
+const textos = document.querySelectorAll(".aba-conteudo");
 // Iterando sobre a lista de botões
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo");
-  };
+    textos[i].classList.add("ativo");
+  }
 }
+
+//document.querySelector("#minhaDiv p").textContent = "Olá Chuva, venha +- com força";
+const contadores  = document.querySelectorAll(".contador");
+const tempoAtual = new Date();// Retorna a data atual do computador
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+
