31304edfd0fb0be0063e05c8185d4897b4bd045c
unknown <kheronn@gmail.com>
2024-04-16 19:42:15 -0300

    R07 - Outros objetivos

--- main.js
+++ main.js
@@ -9,24 +9,41 @@ for (let i = 0; i < botoes.length; i++) {
     }
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
-  }
+  };
 }
 
 //document.querySelector("#minhaDiv p").textContent = "Olá Chuva, venha +- com força";
-const contadores  = document.querySelectorAll(".contador");
-const tempoAtual = new Date();// Retorna a data atual do computador
+const contadores = document.querySelectorAll(".contador");
+const tempoAtual = new Date(); // Retorna a data atual do computador
 const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
+const tempoObjetivo2 = new Date("2024-08-12T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-20T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+  let tempoFinal = tempoObjetivo - tempoAtual;
 
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias  = Math.floor(horas / 24); 
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
 
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
+for (let i = 0; i < contadores.length; i++) {
+  contadores[i].textContent = calculaTempo(tempos[i]);
+}
