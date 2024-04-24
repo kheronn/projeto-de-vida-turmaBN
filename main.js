8c5d26caf934c977e8cca6f4fee189ef268e124b
unknown <kheronn@gmail.com>
2024-04-24 14:51:41 -0300

    R08 - Auytomatizando a contagem regressiva

--- main.js
+++ main.js
@@ -14,15 +14,19 @@ for (let i = 0; i < botoes.length; i++) {
 
 //document.querySelector("#minhaDiv p").textContent = "Olá Chuva, venha +- com força";
 const contadores = document.querySelectorAll(".contador");
-const tempoAtual = new Date(); // Retorna a data atual do computador
-const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
+const tempoObjetivo1 = new Date("2024-04-02T00:00:00");
 const tempoObjetivo2 = new Date("2024-08-12T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
 const tempoObjetivo4 = new Date("2024-06-20T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
+  const tempoAtual = new Date(); // Retorna a data atual do computador
   let tempoFinal = tempoObjetivo - tempoAtual;
+
+  // if(tempoFinal < 0){
+  //   return "Prazo finalizado";
+  // } Outra forma de if
 
   let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos / 60);
@@ -32,18 +36,25 @@ function calculaTempo(tempoObjetivo) {
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
-  return (
-    dias +
-    " dias " +
-    horas +
-    " horas " +
-    minutos +
-    " minutos " +
-    segundos +
-    " segundos"
-  );
+
+  // Se usar o if de cima remova as linhas 41, 43, 44 e 45
+  if(tempoFinal > 0){
+    return dias + " dias " +  horas + " horas " + minutos +  " minutos " +  segundos + " segundos";
+  } else{
+    return "Prazo finalizado."
+  }
+    
 }
 
-for (let i = 0; i < contadores.length; i++) {
-  contadores[i].textContent = calculaTempo(tempos[i]);
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
 }
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 500);
+}
+
+comecaCronometro();
